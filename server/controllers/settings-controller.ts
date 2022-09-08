dcff0240ce16845faf901b15739639e48842b2d0
Ilya Amelevich <ilya.amelevich@gmail.com>
2022-09-08 02:03:33 +0200

    Update deps
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- server/controllers/settings-controller.ts
+++ server/controllers/settings-controller.ts
@@ -1,10 +1,9 @@
 import type { Strapi } from '@strapi/strapi';
-import type { Context } from 'koa';
 import pluginId from '../../common/pluginId';
 import { defaultSettings, PluginSettings } from '../../common/settings';
 
 export default ({ strapi }: { strapi: Strapi }) => ({
-  async index(ctx: Context) {
+  async index(ctx) {
     const savedSettings: PluginSettings | null = await strapi
       .store({ type: 'plugin', name: pluginId, key: 'settings' })
       .get();
@@ -14,7 +13,7 @@ export default ({ strapi }: { strapi: Strapi }) => ({
       ctx.send(defaultSettings);
     }
   },
-  async updateSettings(ctx: Context) {
+  async updateSettings(ctx) {
     const newSettings: PluginSettings = ctx.request.body;
     await strapi
       .store({ type: 'plugin', name: pluginId, key: 'settings' })
